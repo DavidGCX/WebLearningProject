ca5afbd6d6c4e598708ecdcf0c6d2ba05af21943
DGCX <c62gao@uwaterloo.ca>
2024-01-17 14:57:02 -0500

    remove some Test

--- 4-detour/controllers/tourController.js
+++ 4-detour/controllers/tourController.js
@@ -1,30 +1,5 @@
 const fs = require('fs');
 const Tour = require('./../models/tourModel');
-// const tours = JSON.parse(
-//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
-// );
-
-// exports.checkID = (req, res, next, val) => {
-//   console.log(`Tour id is: ${val}`);
-//   if (req.params.id * 1 > tours.length) {
-//     return res.status(404).json({
-//       status: 'fail',
-//       message: 'Invalid ID',
-//     });
-//   }
-//   next();
-// };
-
-// exports.checkBody = (req, res, next) => {
-//   console.log(req.body);
-//   if (!req.body.name || !req.body.price) {
-//     return res.status(400).json({
-//       status: 'fail',
-//       message: 'Missing name or price',
-//     });
-//   }
-//   next();
-// };
 
 exports.getTours = (req, res) => {
 	res.status(200).json({
