5583cd213982192283ea07cdb8b0515bf63a56e4
DGCX <c62gao@uwaterloo.ca>
2024-02-29 12:52:32 -0500

    tour upload photos

--- 4-detour/controllers/tourController.js
+++ 4-detour/controllers/tourController.js
@@ -1,7 +1,30 @@
+const multer = require('multer');
+const sharp = require('sharp');
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 const Factory = require('./handlerFactory');
 const AppError = require('../utils/appError');
+const multerStorage = multer.memoryStorage();
+
+// filter out unwanted files: for this is image,
+// can be used for other file types
+const multerFilter = (req, file, cb) => {
+	if (file.mimetype.startsWith('image')) {
+		cb(null, true);
+	} else {
+		cb(new AppError('Not an image! Please upload only images.', 400), false);
+	}
+};
+
+const upload = multer({
+	storage: multerStorage,
+	fileFilter: multerFilter,
+});
+
+exports.uploadTourImages = upload.fields([
+	{ name: 'imageCover', maxCount: 1 },
+	{ name: 'images', maxCount: 3 },
+]);
 
 exports.aliasTopTours = (req, res, next) => {
 	req.query.limit = '5';
